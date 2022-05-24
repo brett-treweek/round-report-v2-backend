73028179e7ce3dee72b363d2fed5a906676ed271
Brett Treweek <bretttrew@gmail.com>
2022-05-25 00:02:19 +0800

    setup jwt, error handling

---
+++ models/User.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please provide your name'],
+		minlength: 3,
+		maxlength: 20,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide your email'],
+		validate: {
+			validator: validator.isEmail,
+			message: 'Please provide a valid email',
+		},
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide your password'],
+		minlength: 6,
+		select: false
+	},
+	admin: {
+		type: Boolean,
+		default: false,
+		select: false
+	},
+	hazards: [],
+});
+
+UserSchema.pre('save', async function () {
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+	return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_LIFETIME,
+	});
+};
+
+export default mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -2,40 +2,41 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import 'express-async-errors';
 
 // Db and authentication
 import connectDb from './db/connect.js';
 
 // Routers
-import authRouter from './routes/authRoutes.js'
-import hazardsRouter from './routes/hazardsRoutes.js'
+import authRouter from './routes/authRoutes.js';
+import hazardsRouter from './routes/hazardsRoutes.js';
 
 // Middleware
-import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Welcome!');
 });
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/hazards', hazardsRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/hazards', hazardsRouter);
 // handling unfound routes
 app.use(notFoundMiddleware);
 // handling errors in routes
 app.use(errorHandlerMiddleware);
-
+console.log('hello');
 const port = process.env.PORT || 5000;
-
-
 
 const start = async () => {
 	try {
 		await connectDb(process.env.MONGO_URL);
 		app.listen(port, () =>
-			console.log(`Server is listening on port ${port} and is connected to Db...`)
+			console.log(
+				`Server is listening on port ${port} and is connected to Db...`
+			)
 		);
 	} catch (error) {
 		console.log(error);
