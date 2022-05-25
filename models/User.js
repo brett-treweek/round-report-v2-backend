8378311954bbff6adc025358965c7ff05bfa8c54
Brett Treweek <bretttrew@gmail.com>
2022-05-25 12:49:01 +0800

    login/register functionality working

--- models/User.js
+++ models/User.js
@@ -3,38 +3,45 @@ import validator from 'validator';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'Please provide your name'],
-		minlength: 3,
-		maxlength: 20,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: [true, 'Please provide your email'],
-		validate: {
-			validator: validator.isEmail,
-			message: 'Please provide a valid email',
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Please provide your name'],
+			minlength: 3,
+			maxlength: 20,
+			trim: true,
 		},
-		unique: true,
+		email: {
+			type: String,
+			required: [true, 'Please provide your email'],
+			validate: {
+				validator: validator.isEmail,
+				message: 'Please provide a valid email',
+			},
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, 'Please provide your password'],
+			minlength: 6,
+			select: false,
+		},
+		admin: {
+			type: Boolean,
+			default: false,
+			select: false,
+		},
+		round: {
+			type: Number,
+			default: null,
+		},
+		hazards: [],
 	},
-	password: {
-		type: String,
-		required: [true, 'Please provide your password'],
-		minlength: 6,
-		select: false
-	},
-	admin: {
-		type: Boolean,
-		default: false,
-		select: false
-	},
-	hazards: [],
-});
+	{ timestamps: true }
+);
 
 UserSchema.pre('save', async function () {
 	const salt = await bcrypt.genSalt(10);
@@ -45,6 +52,11 @@ UserSchema.methods.createJWT = function () {
 	return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
 		expiresIn: process.env.JWT_LIFETIME,
 	});
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+	const isMatch = await bcrypt.compare(candidatePassword, this.password);
+	return isMatch;
 };
 
 export default mongoose.model('User', UserSchema);
